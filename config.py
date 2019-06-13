2de2ef424b6928a5a7ad75f0588939f8031c6c84
Jyotirmoy Roy <posts.dir@gmail.com>
2019-06-14 09:32:51 +1000

    fixed some pep8 conventions.

---
+++ config.py
@@ -0,0 +1,10 @@
+import pyrebase
+
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://databaseName.firebaseio.com",
+  "storageBucket": "projectId.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
